62dd46a5df92a2af8b5df69ad42f3105f3fd8881
Tai Tran <tai.tran@mgisolutions.com>
2023-09-06 17:30:55 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,8 @@
+// Do not write code directly here, instead use the `src` folder!
+// Then, use this file to export everything you want your user to access.
+
+// @ts-expect-error
+export {default as MyComponent} from './src/MyComponent.astro';
+// @ts-expect-error
+export {default as Button} from './src/components/astro/Button.astro';
+
